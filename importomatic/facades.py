77f2ccdee91fb3c4856c507cc3bd7e9305806200
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2011-12-08 18:39:46 +0100

    added an __str__ method to XmlFacade

--- importomatic/facades.py
+++ importomatic/facades.py
@@ -13,20 +13,24 @@ class XmlFacade(BaseFacade):
     """Xml file wrapper to access it's properties passed to
     :meth:`DefaultConfig.populate`"""
 
-    def __init__(self, item):
+    def __init__(self, item, path):
         self.item = item
+        self.path = path
 
     @classmethod
     def items(cls, file_path, f):
         xml = etree.parse(f)
         root = xml.getroot()
-        return [cls(root)]
+        return [cls(root, file_path)]
 
     def __getattribute__(self, attribute):
         try:
             return super(XmlFacade, self).__getattribute__(attribute)
         except AttributeError:
             return self.item.find(attribute)
+
+    def __str__(self):
+        return '<%s %s>' % (type(self).__name__, self.path)
 
 
 class JsonFacade(BaseFacade):
