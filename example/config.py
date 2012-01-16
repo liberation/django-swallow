4b98b482bf5345a91bd0d6a25472ae3818977335
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2012-01-16 16:00:49 +0100

    updated example config

--- example/config.py
+++ example/config.py
@@ -1,24 +1,58 @@
-from importomatic.config import DefaultConfig
-from importomatic.facades import XmlFacade
+from lxml import etree
 
-from example.models import Copy
+from swallow.config import DefaultConfig
+from swallow.mappers import BaseMapper
+from swallow.populator import BasePopulator
+from swallow.builder import BaseBuilder
+
+from example.models import FeedItem
 
 
-class AtomConfigFacade(XmlFacade):
+NS = {'n':'http://www.w3.org/2005/Atom'}
 
-    @property
-    def instance_filters(self):
-        return {'title': self.path}
+class FeedBuilder(BaseBuilder):
+
+    Model = FeedItem
+
+    class Mapper(BaseMapper):
+
+        def __init__(self, item):
+            self.item = item
+
+        @classmethod
+        def _iter_mappers(cls, file_path, f):
+            xml = etree.parse(f)
+            root = xml.getroot()
+            for item in root.xpath('.//n:entry', namespaces=NS):
+                yield cls(item)
+
+        @property
+        def _instance_filters(self):
+            return {'title': self.title}
+
+        @property
+        def title(self):
+            return self.item.xpath('.//n:title', namespaces=NS)[0].text[:255]
+
+        @property
+        def content(self):
+            return self.item.xpath('.//n:content', namespaces=NS)[0].text
+
+    class Populator(BasePopulator):
+
+        _fields_one_to_one = ('title', 'content')
+        _fields_if_instance_already_exists = None
+        _fields_if_instance_modified_from_last_import = None
+
+    def instance_is_locally_modified(self, instance):
+        return False
+
+    def skip(self, mapper):
+        return False
 
 
 class Github(DefaultConfig):
 
-    def match(self, file_path):
-        return file_path.endswith('.atom')
-
-    model = Copy
-
-    Facade = AtomConfigFacade
-
-    def process(self, facade, instance):
-        return
+    def load_builder(self, path, f):
+        if path.endswith('.atom'):
+            return FeedBuilder(path, f)
