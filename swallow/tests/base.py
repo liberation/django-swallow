16d80ca0e66458a1aff66adefc8f37372c23fd5d
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2012-01-30 17:31:05 +0100

    finish transaction support with minimal tests

---
+++ swallow/tests/base.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+
+from django.test import TransactionTestCase
+
+from swallow import settings
+
+
+CURRENT_PATH = os.path.dirname(__file__)
+
+
+class BaseSwallowTests(TransactionTestCase):
+
+    def setUp(self):
+        settings.SWALLOW_DIRECTORY = os.path.join(CURRENT_PATH, 'import')
+
+        self.import_dir = os.path.join(CURRENT_PATH, 'import')
+        if os.path.exists(self.import_dir):
+            shutil.rmtree(self.import_dir)
+        import_initial = os.path.join(CURRENT_PATH, 'import.initial')
+        shutil.copytree(import_initial, self.import_dir)
+
+    def tearDown(self):
+        shutil.rmtree(self.import_dir)
---
+++ swallow/tests/transactions.py
@@ -0,0 +1,59 @@
+import os
+import re
+
+from base import BaseSwallowTests
+
+from . import Article
+from integration import ArticleMapper
+from integration import ArticlePopulator
+from integration import ArticleBuilder
+from integration import ArticleConfig
+from integration import setup_matchings_and_sections
+
+class TransactionsTests(BaseSwallowTests):
+
+    def test_rollback_one_to_one(self):
+        """Tests that if a one to one affectation fails
+        the transaction rolled back and that other instances
+        are properly saved"""
+
+        setup_matchings_and_sections()
+
+        class ArticleMapperFailsOnOneMapper(ArticleMapper):
+
+            @property
+            def title(self):
+                title = self._item.find('title').text
+                if title  == 'Article Ski':
+                    raise Exception('custom exception')
+                return title
+
+        class BuilderFailsOnOneMapper(ArticleBuilder):
+
+            Mapper = ArticleMapperFailsOnOneMapper
+            Model = Article
+            Populator = ArticlePopulator
+
+        class ConfigFailsOnOneMapper(ArticleConfig):
+
+            def load_builder(self, partial_file_path, fd):
+                filename = os.path.basename(partial_file_path)
+                if re.match(r'^\w+\.xml$', filename) is not None:
+                    return BuilderFailsOnOneMapper(partial_file_path, fd, self)
+                return None
+
+        config = ConfigFailsOnOneMapper()
+        config.run()
+
+        # There is only two articles that made it, no more and *no less*
+        self.assertEqual(2, Article.objects.count())
+
+        # Check that the article in db are the one we expect
+        for article in Article.objects.all():
+            self.assertIn(article.title, ['Article Boxe', 'Article Bilboquet'])
+
+        # check that there is the xml file for Article is in error
+        error = config.error_dir()
+        dir = os.listdir(error)
+        self.assertEqual(1, len(dir))
+        self.assertEqual('ski.xml', dir[0])
