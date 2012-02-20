a11bf66f978e40acdd73b31339a60b4dbe9927cb
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2012-02-20 15:11:26 +0100

    put __init__ in BaseMapper and _iter_mappers takes a builder as argument

--- swallow/mappers.py
+++ swallow/mappers.py
@@ -4,6 +4,10 @@ import json
 
 class BaseMapper(object):
 
+    def __init__(self, item, path=None):
+        self._item = item
+        self._path = path
+
     @property
     def _instance_filters(self):
         """Should return a dictionnary used to get or create
@@ -15,15 +19,12 @@ class XmlMapper(BaseMapper):
     """Xml file mapper to access it's properties passed to
     :meth:`BaseConfig.populate`"""
 
-    def __init__(self, item, path):
-        self._item = item
-        self._path = path
-
     @classmethod
-    def _iter_mappers(cls, file_path, f):
-        xml = etree.parse(f)
+    def _iter_mappers(cls, builder):
+        """The builder should have a fd property"""
+        xml = etree.parse(builder.fd)
         root = xml.getroot()
-        return [cls(root, file_path)]
+        return [cls(root, builder.content)]
 
     def __str__(self):
         return '<%s %s>' % (type(self).__name__, self._path)
