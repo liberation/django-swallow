21bd6724ec7697b4188bbad345595d8a8ea99989
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2012-03-13 20:08:49 +0100

    removed a useless return

--- swallow/populator.py
+++ swallow/populator.py
@@ -82,7 +82,6 @@ class BasePopulator(object):
                     return True
         else:
             return True
-        return False
 
     def _matching_values(self, name):
         """Return matching values for the given Matching, the computation
