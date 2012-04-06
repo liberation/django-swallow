ac9f911644b38e348b628eb310940a9efc5b3e61
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2012-04-06 21:26:55 +0200

    swallow_clean: make it possible to specify several configs and several directories

--- swallow/management/commands/swallow_clean.py
+++ swallow/management/commands/swallow_clean.py
@@ -9,21 +9,41 @@ from swallow.util import get_config
 
 
 class Command(BaseCommand):
-    args = 'import_config_module swallow_dir_name max_age'
-    help = '''Executes specified clean operation on ``swallow_dir``
-            of configuration ``import_config_module``'''
-
+    args = 'import_config_module import_config_module ...'
+    help = ('Executes specified clean operation for provided configuration '
+            'and directories')
     option_list = BaseCommand.option_list + (
         make_option('--dryrun',
             action='store_true',
             dest='dryrun',
             default=False,
-            help="Pretend to do the import but don't do it"),
+            help="Pretend to clean but don't do it"),
+        make_option('--dirs',
+            action='store',
+            dest='dirs',
+            help='Which directory to clean'),
+        make_option('--age',
+            action='store',
+            dest='age',
+            help='Minimum age a file should have to be deleted'),
         )
 
-    def handle(self, *args, **options):
+    def handle(self, *config_module_names, **options):
+        # check that command is properly parametrized
+        if not config_module_names:
+            print 'no configurations provided'
+            return
+        if options['dirs'] is None:
+            print '--dirs is missing'
+            return
+        if options['age'] is None:
+            print '--age is missing'
+            return
+
         dryrun = options['dryrun']
         verbosity = options['verbosity']
+        max_age = int(options['age'])
+        dirs = options['dirs'].split(',')
 
         if dryrun:
             msg = 'This is a dry run. '
@@ -31,25 +51,23 @@ class Command(BaseCommand):
             msg += 'to see what happens'
             self.stdout.write(msg)
 
-        # assign options
-        import_config_module = args[0]
-        swallow_dir = args[1]
-        max_age = int(args[2])
+        for config_module_name in config_module_names:
+            # import config class
+            ConfigClass = get_config(config_module_name)
 
-        # import config class
-        ConfigClass = get_config(import_config_module)
+            for dir_ in dirs:
+                # fetch swallow_dir
+                swallow_dir = '%s_dir' % dir_
+                swallow_dir = getattr(ConfigClass, swallow_dir)()
 
-        # fetch swallow_dir 
-        swallow_dir = getattr(ConfigClass, swallow_dir)()
-
-        # clean dir
-        for dirpath, _, filenames in os.walk(swallow_dir):
-            for filename in filenames:
-                file_path = os.path.join(dirpath, filename)
-                st_mtime = os.stat(file_path).st_mtime
-                age = time() - st_mtime
-                if age > max_age:
-                    if verbosity > 0:  # Use --verbosity=0 to make it quiet
-                        self.stdout.write("%s is to be deleted\n" % file_path)
-                    if not dryrun:
-                        os.remove(file_path)
+                # clean dir
+                for dirpath, _, filenames in os.walk(swallow_dir):
+                    for filename in filenames:
+                        file_path = os.path.join(dirpath, filename)
+                        st_mtime = os.stat(file_path).st_mtime
+                        age = time() - st_mtime
+                        if age > max_age:
+                            if verbosity > 0:  # Use --verbosity=0 to make it quiet
+                                self.stdout.write("%s is to be deleted\n" % file_path)
+                            if not dryrun:
+                                os.remove(file_path)
